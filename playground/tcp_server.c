8a4147baaa8fb702ec2aa6d5e262420acd2b8173
shazilkp <shazilmohemmedkp@gmail.com>
2025-07-30 12:11:02 +0530

    [code] unidirectional tcp client (recv) and server (send) done

---
+++ playground/tcp_server.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+#define PORT "3490"     //string???
+
+#define BACKLOG 10      //server queue size
+
+
+/*
+
+getaddrinfo();          to get port details
+socket();               create socket with the details from getaddrinfo
+setsockopt();           for setting option to allow immediate restart
+bind();                 bind our port to this particular socket
+accept();               accept from listening socket and provide new sockets for connection
+fork() & send();        fork a process and let it handle th new connection
+
+*/
+void sigchld_handler(int s){
+    (void)s;
+
+    int saved_err = errno;
+
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_err;
+}
+
+void *get_in_addr(struct sockaddr *sa){
+    if(sa->sa_family == AF_INET){
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    else{
+        return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    }
+
+}
+
+int main(){
+    int sockfd,new_fd;      //sockfd initial listener, new fd the one communiaction
+    struct addrinfo hints, * servinfo,*p;
+
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes = 1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+
+    memset(&hints,0,sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;        //static ip
+
+    if((rv = getaddrinfo(NULL,PORT,&hints,&servinfo)) != 0){
+        fprintf(stderr,"getaddrinfo: %s\n",gai_strerror(rv));
+        return 1;
+    }
+
+    //loop and bind
+    for(p = servinfo;p != NULL; p = p->ai_next) {
+        if((sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol)) == -1){
+            perror("server: socket");
+            continue;
+        }
+
+        //set option for our socket to available for reuse immediatly
+        if(setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1){
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd,p->ai_addr, p->ai_addrlen) == -1){
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo);
+
+    if(p == NULL){
+        fprintf(stderr,"server : failed to bind\n");
+        exit(1);
+    }
+
+    if(listen(sockfd,BACKLOG) == -1){
+        perror("listen");
+        exit(1);
+
+    }
+
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+    printf("server: waiting for connections...\n");
+
+    while(1){       //working loop
+        sin_size = sizeof(their_addr);
+        new_fd = accept(sockfd,(struct sockaddr *) &their_addr, &sin_size);
+
+        if(new_fd == -1){
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *) &their_addr),s,sizeof(s));
+        printf("server: got connection from %s\n", s);
+
+        if(fork() == 0){            //child process
+            close(sockfd);      //child dont need lstner
+            if(send(new_fd,"Hello to the world of networks",31,0) == -1){
+                perror("send");
+            }
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);
+
+    }
+
+   
+
+    return 0;
+}
